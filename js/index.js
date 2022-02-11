2e69285739e110e123001ee602d62b87802b2e84
keepitclassick <keepitclassick@gmail.com>
2022-02-11 15:39:05 -0500

    add player and methods

--- js/index.js
+++ js/index.js
@@ -3,11 +3,17 @@
 let canvas;
 let ctx;
 
+//game vars
+let gameLoop;
+let player;
+
 //runs when page is loaded
 window.onload = function () {
   canvas = document.getElementById("game-canvas");
   ctx = canvas.getContext("2d");
 
+  //create player
+  player = new Player(100, 400);
   //draw on canvas
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, 1280, 720);
