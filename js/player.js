ee3fefdcff1a6ff8354683853abe08a067f19da7
keepitclassick <keepitclassick@gmail.com>
2022-02-13 21:17:01 -0500

    add jump animation

--- js/player.js
+++ js/player.js
@@ -18,8 +18,8 @@ window.onload = function () {
     moving: false,
   };
 
-  const walk = new Image();
-  walk.src = "./Assets/walk (1).png";
+  const playerSprite = new Image();
+  playerSprite.src = "./Assets/walk (1).png";
 
   const background = new Image();
   background.src = "./Assets/4T_Eqv.png";
@@ -27,44 +27,6 @@ window.onload = function () {
   let drawSprite = function (img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
   };
-
-  /*animate = function () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite(
-      playerSprite,
-      player.width * player.frameX,
-      player.height * player.frameY,
-      player.width,
-      player.height,
-      player.x,
-      player.y,
-      player.width,
-      player.height
-    );
-    movePlayer();
-    handlePlayerFrame();
-    requestAnimationFrame(animate);
-  };
-  animate();*/
-
-  setInterval(function () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-    drawSprite(
-      walk,
-      player.width * player.frameX,
-      player.height * player.frameY,
-      player.width,
-      player.height,
-      player.x,
-      player.y,
-      player.width,
-      player.height
-    );
-    movePlayer();
-    handlePlayerFrame();
-  }, 50);
 
   window.addEventListener("keydown", (e) => {
     keys[e.keyCode] = true;
@@ -79,6 +41,8 @@ window.onload = function () {
     if (keys[38] === true && player.y > 310) {
       player.y -= player.speed;
       player.frameY = 0;
+      console.log(keys);
+      playerSprite.src = "./Assets/jump.png";
     }
 
     if (keys[37] === true && player.x > 0) {
@@ -100,4 +64,24 @@ window.onload = function () {
     if (player.frameX < 7 && player.moving) player.frameX++;
     else player.frameX = 0;
   }
+
+  animate = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    drawSprite(
+      playerSprite,
+      player.width * player.frameX,
+      player.height * player.frameY,
+      player.width,
+      player.height,
+      player.x,
+      player.y,
+      player.width,
+      player.height
+    );
+    movePlayer();
+    handlePlayerFrame();
+    requestAnimationFrame(animate);
+  };
+  animate();
 };
