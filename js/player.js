4f8d7e10b629fc924c32b2e6c91e8ad64e1e197f
keepitclassick <keepitclassick@gmail.com>
2022-02-13 20:47:21 -0500

    update sprite and create walk

--- js/player.js
+++ js/player.js
@@ -2,7 +2,7 @@ window.onload = function () {
   const canvas = document.getElementById("game-canvas");
   const ctx = canvas.getContext("2d");
   canvas.width = 1280;
-  canvas.height = 720;
+  canvas.height = 800;
 
   //var to keep track of keys pressed
   keys = [];
@@ -10,16 +10,16 @@ window.onload = function () {
   const player = {
     x: 200,
     y: 600,
-    width: 100,
-    height: 80,
+    width: 64,
+    height: 128,
     frameX: 0,
     frameY: 0,
     speed: 9,
     moving: false,
   };
 
-  const playerSprite = new Image();
-  playerSprite.src = "./Assets/rsz_spritesheet.png";
+  const walk = new Image();
+  walk.src = "./Assets/walk (1).png";
 
   const background = new Image();
   background.src = "./Assets/4T_Eqv.png";
@@ -28,8 +28,8 @@ window.onload = function () {
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
   };
 
-  animate = function () {
-    //ctx.clearRect(0, 0, canvas.width, canvas.height);
+  /*animate = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     drawSprite(
       playerSprite,
@@ -43,40 +43,61 @@ window.onload = function () {
       player.height
     );
     movePlayer();
+    handlePlayerFrame();
     requestAnimationFrame(animate);
   };
-  animate();
+  animate();*/
+
+  setInterval(function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    drawSprite(
+      walk,
+      player.width * player.frameX,
+      player.height * player.frameY,
+      player.width,
+      player.height,
+      player.x,
+      player.y,
+      player.width,
+      player.height
+    );
+    movePlayer();
+    handlePlayerFrame();
+  }, 50);
 
   window.addEventListener("keydown", (e) => {
     keys[e.keyCode] = true;
-    console.log(keys);
+    player.moving = true;
   });
   window.addEventListener("keyup", (e) => {
     delete keys[e.keyCode];
+    player.moving = false;
   });
 
   function movePlayer() {
     if (keys[38] === true && player.y > 310) {
       player.y -= player.speed;
-      player.frameY = 6;
-      player.frameX = 1;
+      player.frameY = 0;
     }
 
     if (keys[37] === true && player.x > 0) {
       player.x -= player.speed;
-      player.frameY = 5;
+      player.frameY = 0;
     }
 
     if (keys[40] === true && player.y < canvas.height - player.height) {
       player.y += player.speed;
-
-      player.frameY = 4;
+      player.frameY = 0;
     }
 
     if (keys[39] && player.x < canvas.width - player.width) {
       player.x += player.speed;
-
-      player.frameY = 2;
+      player.frameY = 0;
     }
+  }
+  function handlePlayerFrame() {
+    if (player.frameX < 7 && player.moving) player.frameX++;
+    else player.frameX = 0;
   }
 };
